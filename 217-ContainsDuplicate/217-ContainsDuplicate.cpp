1cf50b7902a8b9348f55c0e8f3afd4c8a640105b
Lucky Grover <140267608+GoofyExploit@users.noreply.github.com>
2026-01-03 18:26:21 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -0,0 +1,17 @@
+// Last updated: 1/3/2026, 6:26:20 PM
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int i : nums){
+            mp[i]++;
+        }
+        for(auto& pair : mp){
+            if(pair.second > 1){
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
+};
