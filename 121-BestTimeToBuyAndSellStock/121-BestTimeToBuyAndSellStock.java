3858122b738fd14cd05171bbdb416890a41f0296
Lucky Grover <140267608+GoofyExploit@users.noreply.github.com>
2026-01-03 18:26:42 +0530

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.java
@@ -0,0 +1,11 @@
+// Last updated: 1/3/2026, 6:26:42 PM
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy=Integer.MAX_VALUE,sell=0;
+        for(int i=0;i<prices.length;i++){
+            buy=Math.min(buy,prices[i]);
+            sell=Math.max(sell,prices[i]-buy);
+        }
+       return sell;
+    }
+}
