d52cd7d4295ffa2ac8ddec53e6a1bd161b1a8b0c
Lucky Grover <140267608+GoofyExploit@users.noreply.github.com>
2026-01-03 18:25:53 +0530

    Create 816-DesignHashset

---
+++ 816-DesignHashset/816-DesignHashset.cpp
@@ -0,0 +1,29 @@
+// Last updated: 1/3/2026, 6:25:53 PM
+class MyHashSet {
+private:
+    vector<bool> data;
+public:
+    MyHashSet() {
+        data.resize(1000001, false);
+    }
+    
+    void add(int key) {
+        data[key] = true;
+    }
+    
+    void remove(int key) {
+        data[key] = false;
+    }
+    
+    bool contains(int key) {
+        return data[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
