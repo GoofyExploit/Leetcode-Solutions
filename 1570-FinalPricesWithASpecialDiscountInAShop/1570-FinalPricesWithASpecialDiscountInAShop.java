83d63432d369280512155a5963077b02acb18d18
Lucky Grover <140267608+GoofyExploit@users.noreply.github.com>
2026-01-03 18:25:35 +0530

    Create 1570-FinalPricesWithASpecialDiscountInAShop

---
+++ 1570-FinalPricesWithASpecialDiscountInAShop/1570-FinalPricesWithASpecialDiscountInAShop.java
@@ -0,0 +1,22 @@
+// Last updated: 1/3/2026, 6:25:35 PM
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int answer[] = new int[n];
+
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = prices[i];
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if (prices[j] <= prices[i]){
+                    answer[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
