a05b38aa6c673161258ee7baf0d8d7e37d507f60
Lucky Grover <140267608+GoofyExploit@users.noreply.github.com>
2026-01-04 23:09:18 +0530

    Create 1580-ShuffleTheArray

---
+++ 1580-ShuffleTheArray/1580-ShuffleTheArray.cpp
@@ -0,0 +1,12 @@
+// Last updated: 1/4/2026, 11:09:18 PM
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> arr(2*n);
+        for(int i = 0; i < n; i++){
+            arr[2*i] = nums[i];
+            arr[2*i+1] = nums[i+n];
+        }
+        return arr;
+    }
+};
